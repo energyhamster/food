af1c6ef1fc5b38d0b9ececf8576617bbe86cd1d3
Vlad <vla.opanasenko@gmail.com>
2021-09-18 15:24:47 +0300

    add formdata

--- js/script.js
+++ js/script.js
@@ -228,7 +228,7 @@ document.addEventListener("DOMContentLoaded", () => {
         failure: 'Some thing went wrong...'
     };
 
-    form.forEach(item => {
+    forms.forEach(item => {
         postData(item);
     });
 
